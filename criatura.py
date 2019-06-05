20459f72147272938e897b448267d749cd9af074
mateustranquilino <mateus.tranquilino@ccc.ufcg.edu.br>
2019-06-05 09:55:36 -0300

    Movimento tanque

---
+++ criatura.py
@@ -0,0 +1,27 @@
+
+class Criatura:
+
+    def __init__(self, sprite, pos_x, pos_y, velocidade=20):
+        self.sprite = sprite
+        self.rect = sprite.get_rect()
+        self.rect.x = pos_x
+        self.rect.y = pos_y
+        self.velocidade = velocidade
+
+    def move_left(self):
+        
+        if self.rect.x > -24: 
+            self.rect.x -= self.velocidade
+        
+
+    def move_right(self):
+        
+        if self.rect.x < 896:
+            self.rect.x += self.velocidade 
+    
+
+    def shot(self):
+        pass
+    
+    def move_down(self):
+        pass
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from criatura import Criatura
 import pygame
 import os
 
@@ -13,8 +14,7 @@ print DIRETORIO
 # se a fonte for passada como None é utilizada a padrão do sistema.
 FONTE = pygame.font.Font(DIRETORIO + "/fonts/space_invaders.ttf", 60) 
 BACKGROUND = pygame.image.load(DIRETORIO + "/images/background.jpg")
-NAVE = pygame.image.load(DIRETORIO + "/images/Ship.png")
-RECT = NAVE.get_rect()
+TANQUE = Criatura(pygame.image.load(DIRETORIO + "/images/tank.png"), 436, 590)
 CLOCK = pygame.time.Clock()
 
 # Definido cores
@@ -29,10 +29,10 @@ JANELA = pygame.display.set_mode((1000, 700))
 # Colocando um nome para a janela criada
 pygame.display.set_caption("Space Invaders")
 
-# Variável necessária para que o loop onde o jogo ocorre dure o tempo necessário
-run = True
+
 #JANELA.blit(BACKGROUND, [0,0])
 JANELA.fill((0,0,0))
+
 """
 Criação de outro objeto Surface. O primeiro parâmetro é o texto que será inserido,
 em seguida um booleano indicando se deve ou não suavizar (isso dá a sensação que 
@@ -41,24 +41,35 @@ a imagem é lisa), o terceiro parâmetro é a cor do texto e o último a cor de 
 texto = FONTE.render("SPACE INVADERS", True, VERDE, None)
 JANELA.blit(texto, [220, 50])
 
-RECT.x = 436
-RECT.y = 600
 
-while run:
-    # Coletando os eventos que o usuário está realizando
-    for event in pygame.event.get():
-        # Verificando se o usuário clicou na opção de fechar a janela
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                RECT.x += 15
-            if event.key == pygame.K_LEFT:
-                RECT.x -= 15
+def game():
+    # Variável necessária para que o loop onde o jogo ocorre dure o tempo necessário
+    run = True
+    while run:
+        # Coletando os eventos que o usuário está realizando
+        for event in pygame.event.get():
+            # Verificando se o usuário clicou na opção de fechar a janela
+            if event.type == pygame.QUIT:
+                run = False
 
-    JANELA.blit(NAVE, RECT)
-    pygame.display.flip()
-    CLOCK.tick(60)
+            # Verificando se o usuário pressionou em alguma tecla.
+            if event.type == pygame.KEYDOWN:
+                
+                # Se a tecla pressionada for a seta para direita, move a TANQUE para a direita.
+                if event.key == pygame.K_RIGHT:
+                    TANQUE.move_right()
+                
+                # Se a tecla pressionada for a seta para esquerda, move a TANQUE para a esquerda.
+                if event.key == pygame.K_LEFT:
+                    TANQUE.move_left()
 
-# Comando que encerra os módulos do Pygame
-pygame.quit()
+        
+        JANELA.fill((0,255,0))
+        JANELA.blit(TANQUE.sprite, TANQUE.rect)
+        pygame.display.update()
+        CLOCK.tick(60)
+
+    # Comando que encerra os módulos do Pygame
+    pygame.quit()
+
+game()
