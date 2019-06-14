faf40130ae47ff5df17d21d9846bc6b42989a7d1
mateustranquilino <mateus.tranquilino@ccc.ufcg.edu.br>
2019-06-14 15:44:56 -0300

    'Tenha fé no código que dá certo' - Cajueiro, Adalberto

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 from random import shuffle
 import pygame
+import math
 import os
-
 
 AZUL = (0, 0, 255)
 BRANCO = (255, 255, 255)
@@ -20,38 +20,43 @@ class Criatura:
         self.rect.x = pos_x
         self.rect.y = pos_y
         self.velocidade = velocidade
+        self.ativo = True
 
     def move_left(self):
-
-        if self.rect.left > 10:
+        if self.rect.left > 0:
             self.rect.x -= self.velocidade
 
     def move_right(self):
 
-        if self.rect.right < 1190:
+        if self.rect.right < 1090:
             self.rect.x += self.velocidade
 
-    def shot(self):
-        projetil = Projetil(self.rect.x + self.rect.width / 2, self.rect.y, 1)
+    def shot(self, path):
+        sprite = pygame.transform.scale(pygame.image.load(path), (20,10))
+        projetil = Projetil(sprite, self.rect.midtop, 1)
         return projetil
 
     def move_down(self):
         pass
+
+    def __str__(self):
+        return "Criatura em (%s, %s)" % (self.rect.x, self.rect.y)
     
     
 class Projetil():
 
-    def __init__(self, pos_x, pos_y, direcao, color=BRANCO):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+    def __init__(self, sprite, pos_xy, direcao):
+        self.sprite = sprite
+        self.rect = sprite.get_rect()
+        self.rect.x = pos_xy[0] - 10
+        self.rect.y = pos_xy[1] - 6
         self.direcao = direcao
         self.velocidade = 6 * direcao
-        self.color = color
 
     def draw(self, fundo):
-        pygame.draw.circle(fundo, self.color, (self.pos_x, self.pos_y), 4)
+        fundo.blit(self.sprite, self.rect)
         pygame.display.update()
-        self.pos_y -= self.velocidade
+        self.rect.y -= self.velocidade
 
 
 class SpaceInvaders():
@@ -59,27 +64,20 @@ class SpaceInvaders():
         # Definindo os caminhos dos arquivos necessários para o jogo
         self.DIRETORIO = os.getcwd()
         self.TIRO = None
-        self.LARGURA_TELA = 1200
-        self.ALTURA_TELA = 900
-        self.LIMITE_ESQUERDO = 10
-        self.LIMITE_DIREIRO = 1180
-        self.JANELA = pygame.display.set_mode(
-            (self.LARGURA_TELA, self.ALTURA_TELA))
+        self.LARGURA_TELA = 800
+        self.ALTURA_TELA = 600
+        self.JANELA = pygame.display.set_mode((self.LARGURA_TELA, self.ALTURA_TELA))
         pygame.display.set_caption("Space Invaders")
 
         # Criando um objeto do tipo pygame.font.Font, onde é passada a fonte e o tamanho
         # se a fonte for passada como None é utilizada a padrão do sistema.
-        self.FONTE = pygame.font.Font(
-            self.DIRETORIO + "/fonts/space_invaders.ttf", 60)
-        self.BACKGROUND = pygame.transform.scale(pygame.image.load(
-            self.DIRETORIO + "/images/back.png"), (1200, 900))
-        self.SPRITE_NAVE = pygame.transform.scale(
-            pygame.image.load(self.DIRETORIO + "/images/new/ship.png"), (70, 70))
-        self.NAVE = Criatura(
-            self.SPRITE_NAVE, (self.LARGURA_TELA - 140) / 2, (self.ALTURA_TELA - 100))
+        self.FONTE = pygame.font.Font(self.DIRETORIO + "/fonts/space_invaders.ttf", 60)
+        self.BACKGROUND = pygame.transform.scale(pygame.image.load(self.DIRETORIO + "/images/back.png"), (1200, 900))
+        self.SPRITE_NAVE = pygame.transform.scale(pygame.image.load(self.DIRETORIO + "/images/new/ship.png"), (50, 50))
+        self.NAVE = Criatura(self.SPRITE_NAVE, (self.LARGURA_TELA) / 2, (self.ALTURA_TELA-60))
         self.CLOCK = pygame.time.Clock()
         self.MATRIZ_DE_INIMIGOS = [[], [], [], [], []]
-        self.LINHA_DE_TIRO = self.MATRIZ_DE_INIMIGOS[-1]
+        self.LINHA_DE_TIRO = []
 
     def tela_inicial(self):
         """
@@ -96,8 +94,8 @@ class SpaceInvaders():
         texto = self.FONTE.render("SPACE INVADERS", True, VERDE)
         self.JANELA.blit(texto, [(self.LARGURA_TELA - 550) / 2, 0])
         self.FONTE = pygame.font.Font(self.DIRETORIO + "/fonts/space_invaders.ttf", 30)
-        comando1 = self.FONTE.render(" Para INICIAR o jogo digite I", True, BRANCO, AZUL)
-        comando2 = self.FONTE.render(" Para SAIR do jogo digite S   ", True, BRANCO, AZUL)
+        comando1 = self.FONTE.render(" ENTER ou I: INICIA ", True, BRANCO, AZUL)
+        comando2 = self.FONTE.render(" ESC ou S: ENCERRA", True, BRANCO, AZUL)
         self.JANELA.blit(comando1, ((self.LARGURA_TELA - 550) / 2, self.ALTURA_TELA - 100))
         self.JANELA.blit(comando2, ((self.LARGURA_TELA - 550) / 2, self.ALTURA_TELA - 50))
         pygame.display.update()
@@ -110,77 +108,90 @@ class SpaceInvaders():
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     x, y = pygame.mouse.get_pos()
-                    if (x >= 220 and y >= 550) and (x <= 750 and y <= 590):
+                    if ((x >= ((self.LARGURA_TELA - 550) / 2)) and (y >= (self.ALTURA_TELA - 100))) and ((x <= (self.LARGURA_TELA - 340)) and (y <= (self.ALTURA_TELA - 55))):
                         self.inicia_inimigos()
                         musica_menu.stop()
                         return True
-                    if (x >= 220 and y >= 600) and (x <= 743 and y <= 643):
+                    if ((x >= ((self.LARGURA_TELA - 550) / 2)) and (y >= (self.ALTURA_TELA - 50))) and ((x <= (self.LARGURA_TELA - 340)) and (y <= (self.ALTURA_TELA - 10))):
                         musica_menu.stop()
                         return False
 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_i:
+                    if event.key == pygame.K_i or event.key == pygame.K_RETURN:
                         self.inicia_inimigos()
                         musica_menu.stop()
                         return True
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_s or event.key == pygame.K_ESCAPE:
                         musica_menu.stop()
                         return False
 
     def inicia_inimigos(self):
 
         x = 20
-        y = 60
+        y = 40
         tipos_de_inimigos = []
 
         for i in xrange(1, 8):
-
-            tipos_de_inimigos.append(
-                pygame.image.load(self.DIRETORIO + ("/images/new/test%d.png" % (i % 2))))
+            tipos_de_inimigos.append(pygame.image.load(self.DIRETORIO + ("/images/new/test%d.png" % (i % 2))))
 
         for j in xrange(len(self.MATRIZ_DE_INIMIGOS)):
             linha = self.MATRIZ_DE_INIMIGOS[j]
 
-            for i in xrange(10):
-
-                sprite = pygame.transform.scale(tipos_de_inimigos[j], (50, 50))
+            for i in xrange(8):
+                sprite = pygame.transform.scale(tipos_de_inimigos[j], (40, 40))
                 linha.append(Criatura(sprite, x, y))
 
-                x += 60
+                x += 50
 
-            y += 60
+            y += 40
             x = 20
+
+        for i in xrange(len(self.MATRIZ_DE_INIMIGOS[-1])):
+            self.LINHA_DE_TIRO.append( [self.MATRIZ_DE_INIMIGOS[-1][i], (len(self.MATRIZ_DE_INIMIGOS)-1, i) ] )
 
     def exibe_inimigos(self):
         for linha in self.MATRIZ_DE_INIMIGOS:
             for inimigo in linha:
-                self.JANELA.blit(inimigo.sprite, inimigo.rect)
+                if inimigo.ativo:
+                    self.JANELA.blit(inimigo.sprite, inimigo.rect)
 
     def update(self):
         self.JANELA.blit(self.BACKGROUND, (0, 0))
         self.exibe_inimigos()
         self.JANELA.blit(self.NAVE.sprite, self.NAVE.rect)
+        self.tiro()
+        self.move_inimigos_lateral(1)
         self.CLOCK.tick(60)
-        
-        if self.TIRO is not None:
-            
-            self.TIRO.draw(self.JANELA)
-            for i in xrange(len(self.LINHA_DE_TIRO)):
-                inimigo = self.LINHA_DE_TIRO[i]
-                limite_x, limite_y = inimigo.rect.bottomleft
-                largura_inimigo = inimigo.rect.width
-                if ( self.TIRO.pos_x >= limite_x and self.TIRO.pos_x <= limite_x + largura_inimigo):
+        pygame.display.update()
 
-                    if (self.TIRO.pos_y <= limite_y):
-                        #self.LINHA_DE_TIRO[i].sprite = pygame.transform.scale(pygame.image.load(self.DIRETORIO + "/images/Ship.png" ), (50,50))
-                        self.TIRO = None
-                        break
-                
-            if (self.TIRO is not None and self.TIRO.pos_y < 0):
+    def tiro(self):
+        if self.TIRO is not None:
+            self.TIRO.draw(self.JANELA)
+
+            for dado in self.LINHA_DE_TIRO:
+                inimigo = dado[0]
+                linha, coluna = dado[1]
+                if self.TIRO.rect.colliderect(inimigo.rect):
+                    
+                    self.LINHA_DE_TIRO.remove(dado)
+                    if linha > 0:
+                        self.LINHA_DE_TIRO.append( [self.MATRIZ_DE_INIMIGOS[linha-1][coluna], (linha-1,coluna)] )
+                    
+                    self.MATRIZ_DE_INIMIGOS[linha][coluna].rect.x = -100
+                    self.MATRIZ_DE_INIMIGOS[linha][coluna].ativo = False
+                    self.TIRO = None
+                    break
+
+            if (self.TIRO is not None and self.TIRO.rect.y < 0):
                 self.TIRO = None
 
-        self.exibe_inimigos()
-        pygame.display.update()
+
+    def move_inimigos_lateral(self, direcao):
+        for i in xrange(len(self.MATRIZ_DE_INIMIGOS[0]) -1, -1, -1):
+            for j in xrange(len(self.MATRIZ_DE_INIMIGOS)):
+                inimigo = self.MATRIZ_DE_INIMIGOS[j][i]
+                if inimigo is not None:
+                    inimigo.rect.x += direcao
 
     def main(self):
         # Variável necessária para que o loop onde o jogo ocorre dure o tempo necessário.
@@ -208,8 +219,8 @@ class SpaceInvaders():
                     if event.type == pygame.KEYDOWN:
 
                         if ((event.key == pygame.K_UP or event.key == pygame.K_SPACE) and self.TIRO == None):
-                            self.TIRO = self.NAVE.shot()
-
+                            self.TIRO = self.NAVE.shot(self.DIRETORIO + "/images/laser.png")
+                            self.update()
                         # Se a tecla pressionada for a seta para direita, move a NAVE para a direita.
                         if event.key == pygame.K_RIGHT:
                             self.NAVE.move_right()
@@ -219,7 +230,7 @@ class SpaceInvaders():
                             self.NAVE.move_left()
 
                 self.update()
-                # print (self.NAVE.rect.left), (self.NAVE.rect.right)
+                print (self.NAVE.rect.left), (self.NAVE.rect.right)
 
 if __name__ == "__main__":
     # Comando necessário para se inicializar os módulos do Pygame
